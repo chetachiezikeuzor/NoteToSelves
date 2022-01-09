fb316181739fa4411c086fb1a606b038a484d42a
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-09 14:55:08 -0600

    ✍🏽 Cmd template

---
+++ commands/list.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const auth = require("./auth.json");
+const Scheduler = require("./scheduler");
+
+let bot = new Discord.Client();
+let scheduler = new Scheduler(bot);
+
+exports.run = async (client, message, args) => {
+  let messageContent = message.content.substring(1);
+  let parameters = messageContent.substring(messageContent.indexOf(" ") + 1);
+
+  if (!client.cooldownPhoto) {
+    client.cooldownPhoto = new Set();
+  }
+
+  let cooldownEmbed = new Discord.MessageEmbed()
+    .setAuthor({
+      name: message.author.tag,
+      iconURL: message.author.avatarURL(),
+    })
+    .setColor(process.env.color_pink)
+    .setDescription(
+      `Please wait ${exports.help.cooldown} seconds between commands.`
+    );
+
+  if (client.cooldownPhoto.has(message.author.id))
+    return message.channel.send({ embeds: [cooldownEmbed] });
+
+  client.cooldownPhoto.add(message.author.id);
+  setTimeout(() => {
+    client.cooldownPhoto.delete(message.author.id);
+  }, exports.help.cooldown * 1000);
+
+  scheduler.setReminder(message.author.id, message.channel, parameters);
+};
+
+exports.help = {
+  name: "list",
+  description: "Sets a reminder to ping you at a specific time.",
+  cooldown: "5",
+  usage: "list <id>",
+};
