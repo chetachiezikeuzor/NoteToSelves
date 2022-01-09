5c65d9454ee61c70b6abde842d2b6029a0c51140
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-09 16:21:05 -0600

    🛠 Update cmds

--- commands/noteto.js
+++ commands/noteto.js
@@ -1,12 +1,7 @@
-const moment = require("moment");
-const parser = require("../utils/parser");
 const Discord = require("discord.js");
 const { Scheduler } = require("../utils/scheduler");
 
-let bot = new Discord.Client({
-  disableMentions: "everyone",
-  intents: ["GUILDS", "GUILD_MESSAGES"],
-});
+let bot = new Discord.Client();
 let scheduler = new Scheduler(bot);
 
 exports.run = async (client, message, args) => {
@@ -22,7 +17,7 @@ exports.run = async (client, message, args) => {
       name: message.author.tag,
       iconURL: message.author.avatarURL(),
     })
-    .setColor(process.env.color_blue)
+    .setColor(process.env.color_pink)
     .setDescription(
       `Please wait ${exports.help.cooldown} seconds between commands.`
     );
@@ -35,37 +30,7 @@ exports.run = async (client, message, args) => {
     client.cooldownPhoto.delete(message.author.id);
   }, exports.help.cooldown * 1000);
 
-  if (!parser.validReminderString(message)) {
-    await channel.send(genericParserErrorMessage);
-    return;
-  }
-
-  let reminder = parser.getMessageAndDateFromReminderString(message);
-  let reminderTime = moment(reminder.date);
-
-  agenda.schedule(reminder.date, reminderJobName, {
-    userId: message.author.id,
-    reminder: reminder.message,
-  });
-
-  let embed = new Discord.MessageEmbed()
-    .setAuthor({
-      name: message.author.tag,
-      iconURL: message.author.avatarURL(),
-    })
-    .setColor(process.env.color_blue)
-    .setTitle(
-      `On **${reminderTime.format(dateFormatString)}** I will remind you **${
-        reminder.message
-      }**`
-    )
-    .setColor(process.env.color_blue)
-    .setTimestamp()
-    .setThumbnail(client.user.avatarURL());
-
-  message.channel.send({ embeds: [embed] });
-
-  log(`reminder set for user ${message.author.id}`);
+  scheduler.setReminder(message.author.id, message.channel, parameters);
 };
 
 exports.help = {
