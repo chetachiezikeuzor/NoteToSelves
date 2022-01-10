32db199355194637391a4676b732d66df9e64a9a
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-09 19:18:09 -0600

    🛠 Slash commands in the works

--- events/interaction.js
+++ events/interaction.js
@@ -1,13 +1,16 @@
+const commandFiles = fs.readdirSync("./commands/");
+const commands = [],
+  data = [];
+
 module.exports = (client, interaction) => {
   if (!interaction.isCommand()) return;
-  if (!interaction.user) return;
-  if (!interaction.guild) return;
 
-  const command = interaction.commandName;
-
-  const cmd = client.commands.get(command);
-
-  if (!cmd) return;
-
-  cmd.run(interaction);
+  for (const command of commands) {
+    if (interaction.commandName === command.data.name) {
+      console.log(
+        `${interaction.user.username} ran command ${command.data.name}.`
+      );
+      command.run(interaction);
+    }
+  }
 };
