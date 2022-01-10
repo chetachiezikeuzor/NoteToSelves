801653d96ea130262b1d93a4a8abbff98827704d
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-10 00:38:28 -0600

    🛠 Fix reply

--- commands/list.js
+++ commands/list.js
@@ -5,13 +5,15 @@ const userSchema = require("../models/user");
 exports.run = async (client, message, args) => {
   userSchema.findById(message.author.id).then((u) => {
     if (!u) {
-      message.reply({ embeds: embeds.noReminders() });
+      message.reply({ embeds: [embeds.noReminders()] });
     } else {
       console.log(u.reminders);
       if (u.reminders.length == 0)
         message.reply({ embeds: [embeds.noReminders()] });
       else
-        message.reply({ embeds: embeds.remindersList(u.reminders, u.offset) });
+        message.reply({
+          embeds: [embeds.remindersList(u.reminders, u.offset)],
+        });
     }
   });
 };
