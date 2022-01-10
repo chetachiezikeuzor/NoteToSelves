62293459ff535e87f5b7b0c3e965a436a08a1fa1
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-09 22:54:58 -0600

    🛠 Reconfigure list

--- commands/timezonelist.js
+++ commands/timezonelist.js
@@ -36,14 +36,15 @@ function getTimezoneOffset(timeZone) {
 
 exports.run = async (client, message, args) => {
   let embed = new Discord.MessageEmbed()
+    .setColor(process.env.color_gray)
     .setTitle("Timezone Usage")
     .setDescription(
-      `**Usage:**\n\`n!timezone <number>\`\/\n\n**Example:**\n\`n!timezone -4\`
-    \n You can use one of the popular timezones below, otherwise click ![here](https://gist.github.com/JellyWX/913dfc8b63d45192ad6cb54c829324ee)`
+      `**Usage:**\n\`n!timezone <number>\`\n\n**Example:**\n\`n!timezone -4\`
+    \n You can use one of the popular timezones below, otherwise click [here](https://gist.github.com/JellyWX/913dfc8b63d45192ad6cb54c829324ee)`
     );
 
   timeZonesList.forEach((timeZone) => {
-    embed.addField(`${timeZone}`, `⏱ ${getTimezoneOffset(timeZone)}`, true);
+    embed.addField(`${timeZone}`, `🕝 ${getTimezoneOffset(timeZone)}`, true);
   });
   userSchema.findById(message.author.id).then((u) => {
     if (!u) {
