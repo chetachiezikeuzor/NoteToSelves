d3934b62cb23fabb22e429a711bca71e2328b8ee
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-10 07:17:15 -0600

    ✏️ Change presence

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,11 @@ module.exports = (client) => {
   );
   console.log(`Logged in as ${client.user.tag} (ID: ${client.user.id})\n`);
   client.user.setStatus("available");
-  client.user.setActivity(process.env.activityMessage, {
-    type: process.env.activityType,
+  client.user.setPresence({
+    status: "online",
+    game: {
+      name: process.env.activityMessage,
+      type: process.env.activityType,
+    },
   });
 };
