92de0a5f7b03a38d2d976fad4b43fb377285e897
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-10 14:17:26 -0600

    🛠 Add list /cmd

---
+++ coms/list.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const embeds = require("../embeds");
+const userSchema = require("../models/user");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("list")
+    .setDescription("Lists all active reminders."),
+  async execute(interaction) {
+    const { options, user } = interaction;
+    if (interaction)
+      userSchema.findById(user.id).then(async (u) => {
+        if (!u) {
+          await interaction.reply({ embeds: [embeds.noReminders()] });
+        } else {
+          console.log(u.reminders);
+          if (u.reminders.length == 0)
+            await interaction.reply({ embeds: [embeds.noReminders()] });
+          else
+            await interaction.reply({
+              embeds: [embeds.remindersList(u.reminders, u.offset)],
+            });
+        }
+      });
+  },
+};
