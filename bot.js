71cd9fa7444887f69f0551d554e46d7ce18649b2
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2022-01-11 10:10:53 -0600

    ✏️ Readd cmds

--- bot.js
+++ bot.js
@@ -47,7 +47,7 @@ const rest = new REST({ version: "9" }).setToken(process.env.token);
         `${process.env.clientId}`,
         `${process.env.sw_guildId}`
       ),
-      { body: [] }
+      { body: commands }
     );
 
     await rest.put(
@@ -55,7 +55,7 @@ const rest = new REST({ version: "9" }).setToken(process.env.token);
         `${process.env.clientId}`,
         `${process.env.c_guildId}`
       ),
-      { body: [] }
+      { body: commands }
     );
 
     console.log(`[Commands] Loaded ${commands.length} commands!`);
